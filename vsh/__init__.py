dc1de30dcbf30b7352061093d4a659975c62e57a
Charlotte Connor <charlotte@hrzn.ee>
2023-03-20 22:24:58 +1100

    ci: fix (no pyglet)

--- vsh/__init__.py
+++ vsh/__init__.py
@@ -1,3 +1,11 @@
-from .display import run
+try:
+    import pyglet  # noqa: F401
+
+    from .display import run
+except ModuleNotFoundError:
+
+    def run(_args):
+        print("pyglet not found. Please `pip install pyglet` to use vsh.")
+
 
 __all__ = ["run"]
