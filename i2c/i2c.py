eac6d71af3db196bfa4dc55f61822aac1de608ff
Charlotte Connor <charlotte@hrzn.ee>
2023-03-22 17:25:52 +1100

    counter: factor out

--- i2c/i2c.py
+++ i2c/i2c.py
@@ -10,7 +10,7 @@ from amaranth_boards.resources import (
     I2CResource,  # pyright: reportUnknownVariableType=false
 )
 
-import sim
+from minor import Counter
 
 __all__ = ["I2C", "Speed"]
 
@@ -62,9 +62,6 @@ class I2C(Elaboratable):
     sda_oe: Signal
     sda_i: Signal
 
-    __clocking: Signal
-    __clk_counter_max: int
-    __clk_counter: Signal
     byte: Signal
     byte_ix: Signal
 
@@ -83,7 +80,6 @@ class I2C(Elaboratable):
         self.assign(scl=Pin(1, "io"), sda=Pin(1, "io"))
         self.sda_i.reset = 1
 
-        self.__clocking = Signal()
         self.byte = Signal(8)
         self.byte_ix = Signal(range(7))
 
@@ -135,31 +131,13 @@ class I2C(Elaboratable):
         if plat_i2c is not None:
             self.assign(scl=cast(Pin, plat_i2c.scl), sda=cast(Pin, plat_i2c.sda))
 
-        freq = (
-            cast(int, platform.default_clk_frequency)
-            if platform
-            else int(1 / sim.clock())
-        )
-        self.__clk_counter_max = int(freq // (self.speed.hz * 2))
-        self.__clk_counter = Signal(range(self.__clk_counter_max))
-        # TODO: Timer
-
         m.d.comb += self.scl_oe.eq(1)
+        # NOTE(Mia): we might need to keep scl_o=0 and toggle scl_oe instead for
+        # clock stretching?
 
-        with m.If(self.__clocking):
-            with m.If(self.__clk_counter < self.__clk_counter_max - 1):
-                m.d.sync += self.__clk_counter.eq(self.__clk_counter + 1)
-            with m.Else():
-                m.d.sync += self.__clk_counter.eq(0)
-                m.d.sync += self.scl_o.eq(~self.scl_o)
-
-        half_clock_tgt = int(self.__clk_counter_max // 2)
-        full_clock_tgt = self.__clk_counter_max - 1
-        assert (
-            0 < half_clock_tgt < full_clock_tgt
-        ), f"cannot clock at {self.speed}Hz with {freq}Hz clock; !(0 < {half_clock_tgt} < {full_clock_tgt})"
-        HALF_CLOCK = self.__clk_counter == half_clock_tgt
-        FULL_CLOCK = self.__clk_counter == full_clock_tgt
+        m.submodules.c = c = Counter(hz=self.speed.hz * 2)
+        with m.If(c.o_full):
+            m.d.sync += self.scl_o.eq(~self.scl_o)
 
         with m.FSM():
             with m.State("IDLE"):
@@ -170,8 +148,7 @@ class I2C(Elaboratable):
                 with m.If(self.i_stb & self.fifo.r_rdy):
                     m.d.sync += self.o_busy.eq(1)
                     m.d.sync += self.sda_o.eq(0)
-                    m.d.sync += self.__clk_counter.eq(0)
-                    m.d.sync += self.__clocking.eq(1)
+                    m.d.sync += c.en.eq(1)
 
                     m.d.sync += self.byte.eq((self.i_addr << 1) | self.i_rw)
                     m.d.sync += self.byte_ix.eq(0)
@@ -180,7 +157,7 @@ class I2C(Elaboratable):
                     # This edge: SDA goes low.
 
             with m.State("START"):
-                with m.If(FULL_CLOCK):
+                with m.If(c.o_full):
                     m.next = "DATA"
                     # This edge: SCL goes low.
 
@@ -192,14 +169,14 @@ class I2C(Elaboratable):
                 m.next = "DATA"
 
             with m.State("DATA"):
-                with m.If(HALF_CLOCK):
+                with m.If(c.o_half):
                     # Next edge: SCL goes high -- send bit. (MSB)
                     m.d.sync += self.sda_o.eq((self.byte >> (7 - self.byte_ix)) & 0x1)
-                with m.Elif(FULL_CLOCK):
+                with m.Elif(c.o_full):
                     m.next = "DATA_L"
 
             with m.State("DATA_L"):
-                with m.If(FULL_CLOCK):
+                with m.If(c.o_full):
                     with m.If(self.byte_ix < 7):
                         m.d.sync += self.byte_ix.eq(self.byte_ix + 1)
                         m.next = "DATA"
@@ -209,19 +186,19 @@ class I2C(Elaboratable):
                         # This edge: SCL goes low. Wait for next SCL^ before R/W.
 
             with m.State("ACK"):
-                with m.If(HALF_CLOCK):
+                with m.If(c.o_half):
                     # Next edge: SCL goes high. Let go of SDA.
                     m.d.sync += self.sda_oe.eq(0)
-                with m.Elif(FULL_CLOCK):
+                with m.Elif(c.o_full):
                     m.next = "ACK_L"
 
             with m.State("ACK_L"):
-                with m.If(HALF_CLOCK):
+                with m.If(c.o_half):
                     # Next edge: SCL goes low -- read ACK.
                     # SDA should be brought low by the addressee.
                     m.d.sync += self.o_ack.eq(~self.sda_i)
                     m.d.sync += self.sda_oe.eq(1)
-                with m.Elif(FULL_CLOCK):
+                with m.Elif(c.o_full):
                     # This edge: SCL goes low.
                     with m.If(self.fifo.r_rdy):
                         m.d.sync += self.fifo.r_en.eq(1)
@@ -237,20 +214,20 @@ class I2C(Elaboratable):
                 m.next = "FIN"
 
             with m.State("FIN"):
-                with m.If(HALF_CLOCK):
+                with m.If(c.o_half):
                     # Next edge: SCL goes high -- bring SDA low.
                     m.d.sync += self.sda_o.eq(0)
-                with m.Elif(FULL_CLOCK):
+                with m.Elif(c.o_full):
                     # This edge: SCL goes high.
                     m.next = "STOP"
 
             with m.State("STOP"):
-                with m.If(HALF_CLOCK):
+                with m.If(c.o_half):
                     # Next edge: we'll stop clocking.  Bring SDA high.
                     m.d.sync += self.sda_o.eq(1)
-                with m.Elif(FULL_CLOCK):
+                with m.Elif(c.o_full):
                     # This edge: stop clocking.  Ensure we keep SCL high.
-                    m.d.sync += self.__clocking.eq(0)
+                    m.d.sync += c.en.eq(0)
                     m.d.sync += self.o_busy.eq(0)
                     m.d.sync += self.scl_o.eq(1)
                     m.next = "IDLE"
--- minor/__init__.py
+++ minor/__init__.py
@@ -1,4 +1,6 @@
 from .button import Button, ButtonWithHold
+from .counter import Counter
 from .debounce import Debounce
+from .timer import Timer
 
-__all__ = ["Button", "ButtonWithHold", "Debounce"]
+__all__ = ["Button", "ButtonWithHold", "Debounce", "Counter", "Timer"]
