0408869915096231ec8650f11cdd2e7f19a67f0b
Charlotte <charlotte@hrzn.ee>
2023-07-27 14:21:47 +1000

    shell.nix: use zig-overlay for Zig master.

--- shell.nix
+++ shell.nix
@@ -5,11 +5,19 @@
     rev = "116f2cef9cdc75a33c49c578d3b93b19e68597a7";
     sha256 = "THrX3H1368OP+SXRb+S+cczvCbXubF/5s50VhrtDQbk=";
   }) {};
+
+  zig-overlay = import (pkgs.fetchFromGitHub {
+    owner = "mitchellh";
+    repo = "zig-overlay";
+    rev = "ad3cdf96799c2c34e283feaf491124655c8edcd0";
+    sha256 = "RNC6awMAIWlCnB0HZTJpie1v+mM9sOg+vFiIgF6r/f8=";
+  }) {};
 in
   pkgs.mkShell {
     name = "sh1107";
     nativeBuildInputs = [
       hdx
-      pkgs.zig
+      zig-overlay.master
+      # XXX Due to https://github.com/ziglang/zig/issues/14569, vsh doesn't build on macOS.
     ];
   }
