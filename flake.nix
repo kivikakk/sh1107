b05e359375f1c8db7cacd5972eee07ebd9b543b7
Charlotte <charlotte@hrzn.ee>
2023-08-02 18:17:50 +1000

    ci: WIP for using nix build/github actions.

--- flake.nix
+++ flake.nix
@@ -27,17 +27,59 @@
   in
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit overlays system;};
-    in {
+      hdx = pkgs.hdx.default;
+      python = hdx.hdx-config.python;
+    in rec {
       formatter = pkgs.alejandra;
 
-      devShells.default = pkgs.mkShell {
+      packages.default = python.pkgs.buildPythonPackage {
         name = "sh1107";
-        nativeBuildInputs = with pkgs; [
-          hdx.default
-          zig.master
-          # XXX Due to https://github.com/ziglang/zig/issues/14569, vsh doesn't
-          # build on macOS.
+
+        format = "pyproject";
+        src = ./.;
+
+        nativeBuildInputs = [
+          python.pkgs.setuptools
+          hdx
+          pkgs.zig.master
         ];
+
+        buildInputs = with pkgs; [
+          zig.master
+        ];
+
+        doCheck = true;
+
+        checkPhase = ''
+          BOARDS=( icebreaker orangecrab )
+          SPEEDS=( 100000 400000 )
+
+          set -euo pipefail
+
+          echo "--- Unit tests."
+          #env CI=1 python -m sh1107 test
+
+          for board in "''${BOARDS[@]}"; do
+            for speed in "''${SPEEDS[@]}"; do
+              echo "--- Building $board @ $speed."
+              #python -m sh1107 build "$board" -s "$speed"
+            done
+          done
+
+          for opts in "-c" "-cfi"; do
+            echo "--- Building Virtual SH1107 ($opts)."
+            python -m sh1107 vsh "$opts"
+          done
+
+          echo "--- Formal verification."
+          python -m sh1107 formal
+
+          echo "--- All passed.""
+        '';
       };
+
+      checks.default = packages.default;
+
+      devShells.default = packages.default;
     });
 }
