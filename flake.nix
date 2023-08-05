5eda93c0c506ca49004c9c41e582d949c7fbf18b
Charlotte <charlotte@hrzn.ee>
2023-08-05 13:00:17 +1000

    flake: nix build on nix-darwin, just.

--- flake.nix
+++ flake.nix
@@ -38,17 +38,41 @@
         format = "pyproject";
         src = ./.;
 
-        nativeBuildInputs = [
-          python.pkgs.setuptools
-          hdx
-          pkgs.zig.master
-        ];
+        nativeBuildInputs =
+          [
+            python.pkgs.setuptools
+            hdx
+            pkgs.zig.master
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.xcbuild
+          ];
 
-        buildInputs = with pkgs; [
-          zig.master
-          pkgs.SDL2
-          pkgs.iconv
-        ];
+        buildInputs = with pkgs;
+          [
+            zig.master
+            pkgs.SDL2
+            pkgs.iconv
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
+            OpenGL
+            ForceFeedback
+            CoreGraphics
+            CoreVideo
+            MetalKit
+            Cocoa
+            IOKit
+            AudioToolbox
+            AppKit
+            CoreAudio
+            CoreHaptics
+            Metal
+            QuartzCore
+            Carbon
+            GameController
+            Foundation
+            Quartz
+          ]);
 
         dontAddExtraLibs = true;
 
@@ -58,31 +82,36 @@
 
         doCheck = true;
 
+        pythonImportsCheck = ["sh1107"];
+
         checkPhase = ''
           BOARDS=( icebreaker orangecrab )
           SPEEDS=( 100000 400000 )
+          export CI=1
 
           set -euo pipefail
 
           echo "--- Unit tests."
-          #env CI=1 python -m sh1107 test
+          python -m sh1107 test
 
           for board in "''${BOARDS[@]}"; do
             for speed in "''${SPEEDS[@]}"; do
               echo "--- Building $board @ $speed."
-              #python -m sh1107 build "$board" -s "$speed"
+              python -m sh1107 build "$board" -s "$speed"
             done
           done
 
           for opts in "-c" "-cfi"; do
             echo "--- Building Virtual SH1107 ($opts)."
             python -m sh1107 vsh "$opts"
+            # On Darwin, nix build may appear to fail, but the binary output looks OK.
+            # A true failure would exit non-zero?
           done
 
           echo "--- Formal verification."
           python -m sh1107 formal
 
-          echo "--- All passed.""
+          echo "--- All passed."
         '';
       };
 
